0251b7b0e1265ddb944239dd72602ff8b7e2c5ba
arul xavier <arulxavier2121@gmail.com>
2022-12-03 02:16:02 +0530

    added filter,pagination,sorting functionality

--- server/features/products/products.router.js
+++ server/features/products/products.router.js
@@ -4,7 +4,23 @@ const Products = require("./products.model");
 const app = express.Router();
 
 app.get("/", async (req, res) => {
-  let products = await Products.find();
+  const { page = 1, limit = 10, orderBy = "id", order = "asc" } = req.query;
+  let products = await Products.find()
+    .sort({
+      [orderBy]: order === "asc" ? 1 : -1,
+    })
+    .skip((page - 1) * limit)
+    .limit(limit);
+  res.send(products);
+});
+
+app.get("/category", async (req, res) => {
+  const { page = 1, limit = 3, filter = "all" } = req.query;
+  let products = await Products.find({
+    $or: [{ category: filter }],
+  })
+    .skip((page - 1) * limit)
+    .limit(limit);
   res.send(products);
 });
 
